349f2eac9e8c2faa23fcb20cadc46112facf561f
Douglas Barbosa <Doug_b_souza@hotmail.com>
2025-03-20 20:48:56 -0300

    Improving the code to be functional

--- code/exports/counting.js
+++ code/exports/counting.js
@@ -1,34 +1,36 @@
 export class Count {
     constructor(box1, box2, box3) {
-        this.hoursElement = box1;
-        this.minutesElement = box2;
-        this.secondsElement = box3;
+        this.hoursElement = box1
+        this.minutesElement = box2
+        this.secondsElement = box3
 
-        this.hours = 0;
-        this.minutes = 0;
-        this.seconds = 0;
+        this.hours = 0
+        this.minutes = 0
+        this.seconds = 0
     }
 
     countUp() {
         setInterval(() => {
-            this.seconds++;
-            if (this.seconds === 60) {
-                this.seconds = 0;
-                this.minutes++;
+            this.seconds++
+            if (this.seconds === 60) 
+            {
+                this.seconds = 0
+                this.minutes++
             }
-            if (this.minutes === 60) {
-                this.minutes = 0;
+            if (this.minutes === 60) 
+            {
+                this.minutes = 0
                 this.hours++;
             }
-            this.updateDisplay();
-        }, 1000);
+            this.updateDisplay()
+        }, 1000)
     }
 
     updateDisplay() {
-        this.hoursElement.textContent = this.hours.toString().padStart(2, '0');
-        this.minutesElement.textContent = this.minutes.toString().padStart(2, '0');
-        this.secondsElement.textContent = this.seconds.toString().padStart(2, '0');
+        this.hoursElement.textContent = this.hours.toString().padStart(2, '0')
+        this.minutesElement.textContent = this.minutes.toString().padStart(2, '0')
+        this.secondsElement.textContent = this.seconds.toString().padStart(2, '0')
 
-        console.log(`Horas: ${this.hours.toString().padStart(2, '0')}, Minutos: ${this.minutes.toString().padStart(2, '0')}, Segundos: ${this.seconds.toString().padStart(2, '0')}`);
+        console.log(`Horas: ${this.hours.toString().padStart(2, '0')}, Minutos: ${this.minutes.toString().padStart(2, '0')}, Segundos: ${this.seconds.toString().padStart(2, '0')}`)
     }
 }
--- code/script.js
+++ code/script.js
@@ -1,10 +1,10 @@
-let box1 = document.getElementById('box1')
-let box2 = document.getElementById('box2')
-let box3 = document.getElementById('box3')
-
 import { Count } from './code/exports/counting.js'
 
+document.addEventListener("DOMContentLoaded", () => {
+    let box1 = document.getElementById('box1')
+    let box2 = document.getElementById('box2')
+    let box3 = document.getElementById('box3')
 
-const count = new Count(box1, box2, box3)
-
-count.countUp()
+    const count = new Count(box1, box2, box3)
+    count.countUp()
+})
