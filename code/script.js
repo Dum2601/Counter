9cb476f9a2c87db925eda44bb8c145385a251d16
Douglas Barbosa <136455750+Dum2601@users.noreply.github.com>
2025-03-21 21:06:40 -0300

    Update script.js
    Making it functional!

--- code/script.js
+++ code/script.js
@@ -1,10 +1,27 @@
 import { Count } from './exports/counting.js'
 
 
-let box1 = document.getElementById('box1')
-let box2 = document.getElementById('box2')
-let box3 = document.getElementById('box3')
+const box1 = document.getElementById('box1')
+const box2 = document.getElementById('box2')
+const box3 = document.getElementById('box3')
+const resetBtn = document.getElementById('resetBtn')
+const pauseBtn = document.getElementById('pauseBtn')
 
-const count = new Count(box1, box2, box3)
+
+const count = new Count(box1, box2, box3, resetBtn, pauseBtn)
 count.showDisplay()
+
+pauseBtn.addEventListener('click', () => 
+{
+
+    count.pauseButton()
+
+})
+
+resetBtn.addEventListener('click', () => 
+{
+
+    count.resetButton()
+
+})
 
